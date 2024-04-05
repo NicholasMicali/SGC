aa9b6407ac335e5499fb42ec1739a21296879197
Nicholas <nmicali@calpoly.edu>
2024-04-05 16:50:38 -0700

    Project Init

---
+++ react-vite-app/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>SGC Vite + React App!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        This is the react-vite init localhost for the spread goodness challenge!
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ react-vite-app/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDvAIRcSGPJzhmbIw1W8jeIyblrQdXKmnQ",
+  authDomain: "spreadgoodness-72ac1.firebaseapp.com",
+  projectId: "spreadgoodness-72ac1",
+  storageBucket: "spreadgoodness-72ac1.appspot.com",
+  messagingSenderId: "805068639291",
+  appId: "1:805068639291:web:665cc0e6555de57d3ca01e",
+  measurementId: "G-2QJWF7EL3X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
