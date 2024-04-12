23158382a13d3d03a338e87114b5dd8ec0dec378
BigZhenjie <1741zhenjie@gmail.com>
2024-04-12 11:26:17 -0700

    added some responsiveness to login/signup page

--- react-vite-app/src/components/auth/customInput.jsx
+++ react-vite-app/src/components/auth/customInput.jsx
@@ -6,7 +6,7 @@ const CustomInput = ({type, placeholder, value, onChange, id, labelName}) => {
         <div className="flex flex-col gap-1 w-full mt-3">
             <label htmlFor={id} className="self-start">{labelName}</label>
             <input
-            className="rounded-3xl border-[1px] p-2 border-gray-400"
+            className="rounded-3xl border-[1px] p-2 md:p-3 border-gray-400"
             placeholder={placeholder}
             type={type}
             id={id}
