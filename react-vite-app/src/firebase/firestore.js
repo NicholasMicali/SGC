0733d1fe2fa54024002616ee225ec3749912f1a5
Nicholas <nmicali@calpoly.edu>
2024-04-12 23:17:20 -0700

    adding firestore funcitons

--- react-vite-app/src/firebase/firestore.js
+++ react-vite-app/src/firebase/firestore.js
@@ -10,3 +10,40 @@ export const doCreateUserProfile = async (uid, email, name, age, location) => {
     location
   }, { merge: true });  // Use setDoc to merge data
 };
+
+// Not tested yet: creates a new card which is tied to the user by the uid
+export const doCreateCard = async (uid, cardName, url) => {
+  const cardDoc = doc(db, "cards"); // let firebase create its own id
+  return setDoc(cardDoc, {
+    uid,
+    cardName,
+    url
+  }, { merge: true });
+};
+
+// Also not tested yet: creates a new post which is tied to a card by the cid
+export const doCreatePost = async (cid, title, desc, location, images) => {
+  const postDoc = doc(db, "cards"); 
+  return setDoc(postDoc, {
+    cid,
+    title,
+    desc,
+    location,
+    images,
+  }, { merge: true });
+};
+
+// Also not tested: fetches user porfile details into a doc
+export const doFetchUserProfile = async (uid) => {
+  return db.collection("user_profiles").doc(uid).get()
+}
+
+/*
+export const doFetchCards = async () => {
+
+};
+
+export const doFetchFeed = async () => {
+
+};
+*/
