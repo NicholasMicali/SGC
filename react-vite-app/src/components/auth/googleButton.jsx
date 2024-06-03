88fac634bc4fe9f2bcb145869d26674109f2b315
BigZhenjie <1741zhenjie@gmail.com>
2024-06-02 21:27:02 -0700

    chore: Add framer motion to GoogleButton component

--- react-vite-app/src/components/auth/googleButton.jsx
+++ react-vite-app/src/components/auth/googleButton.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const GoogleButton = ({text, onClick, icon}) => {
-    return (
-        <button className="w-full rounded-lg p-1 flex justify-center gap-4 items-center border-[2px] md:h-[50px] md:mb-8" onClick={onClick}>
-            <img src={icon} height={25} width={25}/>
-            <p className=" text-sm">{text}</p>
-        </button>   
-    );
-}
+const GoogleButton = ({ text, onClick, icon }) => {
+  return (
+    <motion.button
+      className="w-full rounded-lg p-1 flex justify-center gap-4 items-center border-[2px] md:h-[50px] md:mb-8"
+      onClick={onClick}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <img src={icon} height={25} width={25} />
+      <p className=" text-sm">{text}</p>
+    </motion.button>
+  );
+};
 
 export default GoogleButton;
