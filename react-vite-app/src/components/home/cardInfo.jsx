77e9ee1836b35658d8b9da8529ba5f6b7fede897
Nicholas <nmicali@calpoly.edu>
2024-06-05 13:42:46 -0700

    more bug fixes

--- react-vite-app/src/components/home/cardInfo.jsx
+++ react-vite-app/src/components/home/cardInfo.jsx
@@ -29,10 +29,9 @@ const CardInfo = ({name, location, miles, people, isSidebar, color, isNarrowScre
 }
 
 return (
-    <div className={color ? "flex flex-row justify-between bg-gradient-to-tr rounded-xl px-2 py-2 from-gradient-light-start via-gradient-light-mid to-gradient-light-end w-full h-1/8 bg-opacity-40" : "flex flex-col justify-between rounded-xl px-4 py-10 w-full h-full bg-opacity-40 border border-2"}>
+    <div className={color ? "flex flex-row justify-between bg-gradient-to-tr rounded-xl px-2 py-2 from-gradient-light-start via-gradient-light-mid to-gradient-light-end w-full h-1/8 bg-opacity-40" : "flex flex-row justify-between rounded-xl px-2 py-2 w-full h-1/8 bg-opacity-40 border border-2"}>
         <div className="flex flex-row justify-center items-center">
             <div className="font-normal text-sm">{name}</div>
-            <div className="text-sm"></div>
             
         </div>
         |
